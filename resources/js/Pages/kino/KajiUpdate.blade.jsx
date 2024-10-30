4b98b96f49d25847ded0d47912ddc4dc8530de75
sota1231 <bacchuszzzzzzyyyyyy@gmail.com>
2024-10-30 20:35:09 +0900

    デプロイエラーコメントアウト

--- resources/js/Pages/kino/KajiUpdate.blade.jsx
+++ resources/js/Pages/kino/KajiUpdate.blade.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Inertia } from '@inertiajs/inertia';
-import { InertiaLink, usePage } from '@inertiajs/inertia-react';
-import Layout from '@/Layouts/Layout1';
+// import React from 'react';
+// import { Inertia } from '@inertiajs/inertia';
+// import { InertiaLink, usePage } from '@inertiajs/inertia-react';
+// import Layout from '@/Layouts/Layout1';
 
-const KajiUpdate = ({ ratios }) => {
-  const { auth, errors } = usePage().props;
+// const KajiUpdate = ({ ratios }) => {
+//   const { auth, errors } = usePage().props;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // フォーム送信の処理をここに実装
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // フォーム送信の処理をここに実装
+//   };
 
-  return (
-    <Layout>
-      {/* ここにコンポーネントの内容を実装 */}
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       {/* ここにコンポーネントの内容を実装 */}
+//     </Layout>
+//   );
+// };
 
-export default KajiUpdate;
+// export default KajiUpdate;
